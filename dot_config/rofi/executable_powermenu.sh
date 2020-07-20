501a655694f8212d70ebc5ae571c7cc2d99cdb35
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2020-07-20 11:36:28 -0400

    new rofi theme, gtk themes, and icons

---
+++ dot_config/rofi/executable_powermenu.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#
+# Powermenu for Rofi
+# 
+# Author : Lucero Alvarado
+# Github : @lu0
+#
+
+# Options as characters
+# Copied from decoded unicodes (private use of "Feather" font)
+shutdown="";        # "\uE9C0"
+reboot="";          # "\uE9C4"
+sleep="";           # "\uE9A3"
+logout="";          # "\uE991"
+lock="";            # "\uE98F"
+options="$shutdown\n$reboot\n$sleep\n$logout\n$lock"
+
+# Fake blurred background
+SS_PATH="$HOME/.config/rofi/screenshot"
+rm -f "${SS_PATH}.jpg" && scrot -z "${SS_PATH}.jpg"                 # screenshot
+convert "${SS_PATH}.jpg" -blur 0x10 -auto-level "${SS_PATH}.jpg"    # blur
+convert "${SS_PATH}.jpg" "${SS_PATH}.png"                           # rofi reads png images
+
+# Font size according to screen dimensions
+DEFAULT_WIDTH=1920
+WIDTH=$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3}')
+DEFAULT_FONTSIZE=24
+FONTSIZE=$(echo "$WIDTH/$DEFAULT_WIDTH*$DEFAULT_FONTSIZE" | bc -l)
+
+while getopts "lp" OPT; do
+    case "$OPT" in
+        p) PRESELECTION=0 ;;
+        l) PRESELECTION=3 ;;
+        *) PRESELECTION=4 ;;
+    esac
+done
+if (( $OPTIND == 1 )); then
+   PRESELECTION=4
+fi
+
+selected="$(echo -e "$options" | 
+            rofi -show-icons -theme powermenu_theme.rasi \
+                 -fake-background ${SS_PATH}.png -fake-transparency \
+                 -font "WeblySleek UI Light, $FONTSIZE" \
+                 -p "See you later, ${LOGNAME^}!" -dmenu -selected-row ${PRESELECTION})"
+
+case $selected in
+
+    $shutdown)
+        systemctl poweroff
+        ;;
+
+    $reboot)
+        systemctl reboot
+        ;;
+
+    $sleep)
+        systemctl suspend
+        ;;
+
+    $logout)
+        i3-msg exit
+        ;;
+
+    $lock)
+        cinnamon-screensaver-command --lock || ( xflock4 || mate-screensaver-command -l )
+        ;;
+
+esac
