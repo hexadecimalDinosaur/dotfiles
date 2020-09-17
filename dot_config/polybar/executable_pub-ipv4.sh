9b4834b5a714e6c5e611809c88a3f63adaf5167b
UserBlackBox <userblackbox@tutanota.com>
2020-09-17 09:50:46 -0400

    script update

--- dot_config/polybar/executable_pub-ipv4.sh
+++ dot_config/polybar/executable_pub-ipv4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dig +short myip.opendns.com @resolver1.opendns.com
-#echo $(curl -s https://ipinfo.io/ip 2> /dev/null)
+#dig +short myip.opendns.com @resolver1.opendns.com 2> /dev/null
+echo $(curl -s https://ipinfo.io/ip 2> /dev/null)
