dd1f0845f4f0e98f9c526d780c9b652db4507cf5
UserBlackBox <userblackbox@tutanota.com>
2020-10-01 09:40:09 -0400

    update mpris script

--- dot_config/polybar/scripts/executable_player-mpris-simple.sh
+++ dot_config/polybar/scripts/executable_player-mpris-simple.sh
@@ -3,7 +3,11 @@
 player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-	echo "$(playerctl metadata --format "{{artist}} - {{title}}" 2> /dev/null)"
+	if [[ $(playerctl metadata --format "{{artist}}" 2> /dev/null) ]]; then
+		echo "$(playerctl metadata --format "{{artist}} - {{title}}" 2> /dev/null)"
+	else
+		echo "$(playerctl metadata --format "{{title}}" 2> /dev/null)"
+	fi
 elif [ "$player_status" = "Paused" ]; then
     echo "Paused"
 else
