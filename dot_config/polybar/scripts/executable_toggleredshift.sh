88a6feb82cc17fd73c91d71d51968bdf89375ead
UserBlackBox <userblackbox@tutanota.com>
2020-11-04 09:47:56 -0500

    update

--- dot_config/polybar/scripts/executable_toggleredshift.sh
+++ dot_config/polybar/scripts/executable_toggleredshift.sh
@@ -4,6 +4,6 @@ if [[ $result == "redshift" ]]
 then
 redshift -x && killall redshift && notify-send "Screen Temperature Reset" -t 2000 
 else
-redshift  -t 3000:3000 &
+redshift -l 43.4:79.2  &
 notify-send "Redshift on" -t 2000
 fi
