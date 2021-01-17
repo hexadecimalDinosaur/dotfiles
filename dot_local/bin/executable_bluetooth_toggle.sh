02f0695ab1f3efe6ff58b50846fa90522c97bdb1
UserBlackBox <userblackbox@tutanota.com>
2021-01-17 14:36:56 -0500

    remove .local/bin

--- dot_local/bin/executable_bluetooth_toggle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rfkill | grep "hci0     blocked" > /dev/null
-if [ $? -eq 0 ]; then
-	rfkill unblock 1
-else
-	rfkill block 1
-fi
--- dot_local/bin/executable_windscribe_toggle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-windscribe status | grep DISCONNECTED > /dev/null
-if [ $? -eq 0 ]; then
-	windscribe connect
-else
-	windscribe disconnect
-fi
